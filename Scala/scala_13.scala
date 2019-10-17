3a5afcd9904c03c746744e732b4087ad028e90d0
Ned <knifezzltu@gmail.com>
2019-10-17 22:34:45 +0300

    updated diagrams

--- Scala/scala_13.scala
+++ Scala/scala_13.scala
@@ -2,7 +2,7 @@ import org.joda.time.DateTime
 import org.joda.time.format._
 
 trait BasicRewardPolicy {
-  protected def CalculateRewardChance (attendancePercent: Double) : Double = {
+  def CalculateRewardChance (attendancePercent: Double) : Double = {
     if (attendancePercent > 90)
       return 98;
     if (attendancePercent > 50)
@@ -13,29 +13,37 @@ trait BasicRewardPolicy {
 }
 
 trait RegularAttendancePolicy {
-  protected def AdjustIfMainActivity (isMain: Boolean, chance: Double) : Double = {
+  def AdjustIfMainActivity (isMain: Boolean, chance: Double) : Double = {
     if (isMain)
       return chance;
     else
       return chance / 2;
   }
 
-  protected def ChanceOfAttendance (isMain: Boolean, chance: Double, distractions: List[String]) : Double = {
+  def ChanceOfAttendance (isMain: Boolean, chance: Double, distractions: List[String]) : Double = {
     var temp = Math.abs(chance - (100 * (0.1 * distractions.length)));
     return AdjustIfMainActivity(isMain, temp);
   }
 
-  protected def CheckUniqueness (distractions: List[String]) : Boolean = {
+  def CheckUniqueness (distractions: List[String]) : Boolean = {
     if (distractions.distinct.size != distractions.size) {
       return false;
     } else {
       return true;
     }
   }
+  
+  def CalculateChanceOfAttendance (distractions: List[String], isMain: Boolean): Double = {
+    var chance: Double = 100;
+    if (CheckUniqueness(distractions))
+        return ChanceOfAttendance(isMain, chance, distractions);
+    else
+        return -1;
+  }
 }
 
 trait YearsInUniversityCalculator {
-  protected def TimeSpentInUniversity (joinedDate: DateTime) : String = {
+  def TimeSpentInUniversity (joinedDate: DateTime) : String = {
     val year = DateTime.now.getYear() - joinedDate.getYear();
     if (year == 0) { return "This is his/hers first year in university"; }
     else if (year == 1) { return "This person has spent 1 year in university"; }
@@ -43,109 +51,34 @@ trait YearsInUniversityCalculator {
   }
 }
 
-abstract class Student {
+class Student {
   private var _joinedDate: DateTime =_;
-  private var _distractions: List[String] =_;
-  private var _attendancePercent: Double =_;
-  private var _isUniversityMainActivity: Boolean = true;
+  var _distractions: List[String] =_;
+  var _attendancePercent: Double =_;
+  var _isUniversityMainActivity: Boolean = true;
 
   def set_joinedDate(x: DateTime): Unit = {_joinedDate = x; }
   def get_joinedDate(): DateTime = { return _joinedDate; }
-  def set_distractions(x: List[String]): Unit = {_distractions = x; }
-  def set_attendancePercent(x: Double): Unit = {_attendancePercent = x; }
-  def set_isUniversityMainActivity(x: Boolean): Unit = {_isUniversityMainActivity = x; }
-
-  protected def CalculateRewardChance (attendancePercent: Double) : Double;
-  protected def AdjustIfMainActivity (isMain: Boolean, chance: Double) : Double
-  protected def ChanceOfAttendance (isMain: Boolean, chance: Double, distractions: List[String]) : Double;
-  protected def CheckUniqueness (distractions: List[String]) : Boolean;
-  protected def TimeSpentInUniversity (joinedDate: DateTime) : String;
-
-  def CalculateChanceOfAttendance (): Double = {
-    var chance: Double = 100;
-    if (CheckUniqueness(_distractions))
-        return ChanceOfAttendance(_isUniversityMainActivity, chance, _distractions);
-    else
-        return -1;
-  }
-
-  def CalculateRewardChance(): Double = {
-    return CalculateRewardChance(_attendancePercent);
-  }
-
-  def TimeSpentInUniversity(): String = {
-    return TimeSpentInUniversity(_joinedDate);
-  }
 }
 
-abstract class Lecturer {
+class Lecturer {
   private var _joinedDate: DateTime =_;
-  private var _distractions: List[String] =_;
-  private var _attendancePercent: Double =_;
-  private var _isUniversityMainActivity: Boolean = true;
+  var _distractions: List[String] =_;
+  var _attendancePercent: Double =_;
+  var _isUniversityMainActivity: Boolean = true;
 
   def set_joinedDate(x: DateTime): Unit = {_joinedDate = x; }
   def get_joinedDate(): DateTime = { return _joinedDate; }
-  def set_distractions(x: List[String]): Unit = {_distractions = x; }
-  def set_attendancePercent(x: Double): Unit = {_attendancePercent = x; }
-  def set_isUniversityMainActivity(x: Boolean): Unit = {_isUniversityMainActivity = x; }
-
-  protected def CalculateRewardChance (attendancePercent: Double) : Double;
-  protected def AdjustIfMainActivity (isMain: Boolean, chance: Double) : Double
-  protected def ChanceOfAttendance (isMain: Boolean, chance: Double, distractions: List[String]) : Double;
-  protected def CheckUniqueness (distractions: List[String]) : Boolean;
-  protected def TimeSpentInUniversity (joinedDate: DateTime) : String;
-
-  def CalculateChanceOfAttendance (): Double = {
-    var chance: Double = 100;
-    if (CheckUniqueness(_distractions))
-        return ChanceOfAttendance(_isUniversityMainActivity, chance, _distractions);
-    else
-        return -1;
-  }
-
-  def CalculateRewardChance(): Double = {
-    return CalculateRewardChance(_attendancePercent);
-  }
-
-  def TimeSpentInUniversity(): String = {
-    return TimeSpentInUniversity(_joinedDate);
-  }
 }
 
-abstract class SecurityGuard {
+class SecurityGuard {
   private var _joinedDate: DateTime =_;
-  private var _distractions: List[String] =_;
-  private var _attendancePercent: Double =_;
-  private var _isUniversityMainActivity: Boolean = true;
+  var _distractions: List[String] =_;
+  var _attendancePercent: Double =_;
+  var _isUniversityMainActivity: Boolean = true;
 
   def set_joinedDate(x: DateTime): Unit = {_joinedDate = x; }
   def get_joinedDate(): DateTime = { return _joinedDate; }
-  def set_distractions(x: List[String]): Unit = {_distractions = x; }
-  def set_attendancePercent(x: Double): Unit = {_attendancePercent = x; }
-  def set_isUniversityMainActivity(x: Boolean): Unit = {_isUniversityMainActivity = x; }
-
-  protected def CalculateRewardChance (attendancePercent: Double) : Double;
-  protected def AdjustIfMainActivity (isMain: Boolean, chance: Double) : Double
-  protected def ChanceOfAttendance (isMain: Boolean, chance: Double, distractions: List[String]) : Double;
-  protected def CheckUniqueness (distractions: List[String]) : Boolean;
-  protected def TimeSpentInUniversity (joinedDate: DateTime) : String;
-
-  def CalculateChanceOfAttendance (): Double = {
-    var chance: Double = 100;
-    if (CheckUniqueness(_distractions))
-        return ChanceOfAttendance(_isUniversityMainActivity, chance, _distractions);
-    else
-        return -1;
-  }
-
-  def CalculateRewardChance(): Double = {
-    return CalculateRewardChance(_attendancePercent);
-  }
-
-  def TimeSpentInUniversity(): String = {
-    return TimeSpentInUniversity(_joinedDate);
-  }
 
   def GetRiskOfBeingFired(): Double = {
     if (_distractions.length == 0) { return 0; }
@@ -157,26 +90,26 @@ object Test {
   def main(args: Array[String]): Unit = {
     var stud = new Student with BasicRewardPolicy with RegularAttendancePolicy with YearsInUniversityCalculator;
     var distractions: List[String] = List("is tired", "lazy", "after vacation");
-    stud.set_distractions(distractions);
+    stud._distractions = distractions;
     stud.set_joinedDate(new DateTime(2017, 9, 9, 0, 0, 0));
-    stud.set_attendancePercent(95);
-    stud.set_isUniversityMainActivity(true);
+    stud._attendancePercent = 95;
+    stud._isUniversityMainActivity = true;
 
-    println(stud.CalculateChanceOfAttendance());
-    println(stud.CalculateRewardChance());
-    println(stud.TimeSpentInUniversity());
+    println(stud.CalculateChanceOfAttendance(stud._distractions, stud._isUniversityMainActivity));
+    println(stud.CalculateRewardChance(stud._attendancePercent));
+    println(stud.TimeSpentInUniversity(stud.get_joinedDate()));
 
     var guard = new SecurityGuard with BasicRewardPolicy with RegularAttendancePolicy with YearsInUniversityCalculator;
     distractions = List("is tired", "lazy");
-    guard.set_distractions(distractions);
+    guard._distractions = distractions;
     guard.set_joinedDate(new DateTime(2015, 9, 9, 0, 0, 0));
-    guard.set_attendancePercent(95);
-    guard.set_isUniversityMainActivity(true);
+    guard._attendancePercent = 95;
+    guard._isUniversityMainActivity = true;
 
     println("=============================================")
-    println(guard.CalculateChanceOfAttendance());
-    println(guard.CalculateRewardChance());
-    println(guard.TimeSpentInUniversity());
+    println(guard.CalculateChanceOfAttendance(guard._distractions, guard._isUniversityMainActivity));
+    println(guard.CalculateRewardChance(guard._attendancePercent));
+    println(guard.TimeSpentInUniversity(guard.get_joinedDate()));
     println("Chance of getting fired is " + guard.GetRiskOfBeingFired());
   }
 }
